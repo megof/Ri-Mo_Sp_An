02251f77cc62118c56664967daebebbd3b3f3b49
David Ospina <J_do_o13@hotmail.com>
2024-09-21 19:23:38 +0200

    init config

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {    
+    production: false,
+    apiURL_users: 'https://rickandmortyapi.com/api',
+    apiURL: 'localhots:8080/api/v1'
+};
